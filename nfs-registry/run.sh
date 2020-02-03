442caa4294500e664938bc25dc6934e52913ad3e
Luis Arizmendi <larizmen@redhat.com>
2020-02-03 15:26:26 +0100

    fix registry install in 4.3

--- nfs-registry/run.sh
+++ nfs-registry/run.sh
@@ -5,6 +5,7 @@ echo "Configuring NFS for Registry"
 echo "****************************"
 
 
+
 oc process -f template.yaml --param-file=env | oc create -f -
 
 
@@ -12,4 +13,5 @@ RESOURCE="cluster"
 while [[ $(oc get configs.imageregistry.operator.openshift.io cluster | grep $RESOURCE  > /dev/null ; echo $?) != "0" ]]; do echo "Waiting for $RESOURCE object" && sleep 10; done
 
 
-oc patch configs.imageregistry.operator.openshift.io cluster --type='json' -p='[{"op": "remove", "path": "/spec/storage" },{"op": "add", "path": "/spec/storage", "value": {"pvc":{"claim": ""}}}]'
+#oc patch configs.imageregistry.operator.openshift.io cluster --type='json' -p='[{"op": "remove", "path": "/spec/storage" },{"op": "add", "path": "/spec/storage", "value": {"pvc":{"claim": ""}}}]'
+oc patch configs.imageregistry.operator.openshift.io cluster --type='json' -p='[{"op": "replace", "path": "/spec/managementState", "value": "Managed" },{"op": "remove", "path": "/spec/storage" },{"op": "add", "path": "/spec/storage", "value": {"pvc":{"claim": ""}}}]'
