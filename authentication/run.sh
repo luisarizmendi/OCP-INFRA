3694224efc015c259cf565549de7906006c4c793
Luis Arizmendi <larizmen@hal9k.local>
2020-05-20 12:09:55 +0200

    fix destination rule in Service Mesh

--- authentication/run.sh
+++ authentication/run.sh
@@ -6,33 +6,33 @@ echo "****************************"
 
 ## Create htpasswd file with users
 #sudo yum install -y httpd-tools
-#htpasswd -c -B -b users.htpasswd clusteradmin redhat
-#htpasswd -b users.htpasswd viewuser redhat
-#htpasswd -b users.htpasswd user1 redhat
-#htpasswd -b users.htpasswd user2 redhat
-#htpasswd -b users.htpasswd user3 redhat
-#htpasswd -b users.htpasswd user4 redhat
-#htpasswd -b users.htpasswd user5 redhat
-#htpasswd -b users.htpasswd user6 redhat
-#htpasswd -b users.htpasswd user7 redhat
-#htpasswd -b users.htpasswd user8 redhat
-#htpasswd -b users.htpasswd user9 redhat
-#htpasswd -b users.htpasswd user10 redhat
-#htpasswd -b users.htpasswd user11 redhat
-#htpasswd -b users.htpasswd user12 redhat
-#htpasswd -b users.htpasswd user13 redhat
-#htpasswd -b users.htpasswd user14 redhat
-#htpasswd -b users.htpasswd user15 redhat
-#htpasswd -b users.htpasswd user16 redhat
-#htpasswd -b users.htpasswd user17 redhat
-#htpasswd -b users.htpasswd user18 redhat
-#htpasswd -b users.htpasswd user19 redhat
-#htpasswd -b users.htpasswd user20 redhat
-#htpasswd -b users.htpasswd user21 redhat
-#htpasswd -b users.htpasswd user22 redhat
-#htpasswd -b users.htpasswd user23 redhat
-#htpasswd -b users.htpasswd user24 redhat
-#htpasswd -b users.htpasswd user25 redhat
+#htpasswd -c -B -b users.htpasswd clusteradmin R3dhat01
+#htpasswd -b users.htpasswd viewuser R3dhat01
+#htpasswd -b users.htpasswd user1 R3dhat01
+#htpasswd -b users.htpasswd user2 R3dhat01
+#htpasswd -b users.htpasswd user3 R3dhat01
+#htpasswd -b users.htpasswd user4 R3dhat01
+#htpasswd -b users.htpasswd user5 R3dhat01
+#htpasswd -b users.htpasswd user6 R3dhat01
+#htpasswd -b users.htpasswd user7 R3dhat01
+#htpasswd -b users.htpasswd user8 R3dhat01
+#htpasswd -b users.htpasswd user9 R3dhat01
+#htpasswd -b users.htpasswd user10 R3dhat01
+#htpasswd -b users.htpasswd user11 R3dhat01
+#htpasswd -b users.htpasswd user12 R3dhat01
+#htpasswd -b users.htpasswd user13 R3dhat01
+#htpasswd -b users.htpasswd user14 R3dhat01
+#htpasswd -b users.htpasswd user15 R3dhat01
+#htpasswd -b users.htpasswd user16 R3dhat01
+#htpasswd -b users.htpasswd user17 R3dhat01
+#htpasswd -b users.htpasswd user18 R3dhat01
+#htpasswd -b users.htpasswd user19 R3dhat01
+#htpasswd -b users.htpasswd user20 R3dhat01
+#htpasswd -b users.htpasswd user21 R3dhat01
+#htpasswd -b users.htpasswd user22 R3dhat01
+#htpasswd -b users.htpasswd user23 R3dhat01
+#htpasswd -b users.htpasswd user24 R3dhat01
+#htpasswd -b users.htpasswd user25 R3dhat01
 
 
 # Assign htpasswd file to auth provisioner and enable provisioner
@@ -55,4 +55,4 @@ oc adm policy add-role-to-group admin developers
 
 
 # Remove kubeadmin
-oc delete secrets kubeadmin -n kube-system
+#oc delete secrets kubeadmin -n kube-system
